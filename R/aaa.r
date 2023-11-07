16e4722ed4fd9bd7f20db345635e44be1de9361d
Joey Couse <joeycouse777@gmail.com>
2023-11-07 10:43:46 -0600

    update onattach

--- R/aaa.r
+++ R/aaa.r
@@ -1,20 +1,24 @@
 .onAttach <- function(libname, pkgname) {
 
+  if("IBM Plex Sans" %in% systemfonts::system_fonts()$family == FALSE){
+    stop("You need to install the IBM Plex Sans font: https://github.com/IBM/plex/")
+  }
 
-  systemfonts::register_variant(name = "IBM Plex Sans SemiBold",
-                                family = "IBM Plex Sans",
-                                weight = "semibold",
-                                width = "normal")
-
-  systemfonts::register_variant(name = "IBM Plex Sans Light",
+  systemfonts::register_variant(name = "IBM Plex Light",
                                 family = "IBM Plex Sans",
                                 weight = "light",
                                 width = "normal")
 
-  systemfonts::register_variant(name = "IBM Plex Sans Medium",
+  systemfonts::register_variant(name = "IBM Plex Medium",
                                 family = "IBM Plex Sans",
                                 weight = "medium",
                                 width = "normal")
 
+  systemfonts::register_variant(name = "IBM Plex",
+                                family = "IBM Plex Sans",
+                                weight = c("normal", "semibold"),
+                                width = "normal")
+
+ggplot2::theme_set(cousepkg::theme_couse())
 
 }
