477547383622906014167ae3d08b729207c228db
joeycouse <joeycouse777@gmail.com>
2024-07-09 09:31:29 -0500

    fix font exports - Look into this more for different font styles

--- R/aaa.r
+++ R/aaa.r
@@ -4,22 +4,22 @@
     stop("You need to install the Oracle Sans Font")
   }
 
-  systemfonts::register_variant(name = "Oracle Sans Light",
-                                family = "Oracle Sans",
-                                weight = "light",
-                                width = "normal")
-
-  systemfonts::register_variant(name = "Oracle Sans Medium",
-                                family = "Oracle Sans",
-                                weight = "medium",
-                                width = "normal")
-
-  systemfonts::register_variant(name = "Oracle Sans",
-                                family = "Oracle Sans",
-                                weight = c("normal", "semibold"),
-                                width = "normal")
+  # systemfonts::register_variant(name = "Oracle Sans Light",
+  #                               family = "Oracle Sans",
+  #                               weight = "light",
+  #                               width = "normal")
+  #
+  # systemfonts::register_variant(name = "Oracle Sans Medium",
+  #                               family = "Oracle Sans",
+  #                               weight = "normal",
+  #                               width = "normal")
+  #
+  # systemfonts::register_variant(name = "Oracle Sans",
+  #                               family = "Oracle Sans",
+  #                               weight = c("normal", "semibold"),
+  #                               width = "normal")
 
 packageStartupMessage("The default ggplot2 theme has been set to theme_oracle()")
-ggplot2::theme_set(cousepkg::theme_oracle())
+ggplot2::theme_set(oraclepkg::theme_oracle())
 
 }
